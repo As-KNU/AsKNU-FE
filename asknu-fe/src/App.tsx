0af3d55f382dd3118306ec178f837a66f78b8e00
Yea <dbs6463@gmail.com>
2025-11-30 18:25:25 +0900

    tailwind 파일 추가

--- asknu-fe/src/App.tsx
+++ asknu-fe/src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import FaqPage from "./pages/FaqPage";
+import EmployeePage from "./pages/EmployeePage";
 import "./index.css";
 
 export default function App() {
   return (
-    <div className="flex justify-center items-start min-h-screen bg-gray-100 pt-6">
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="mobile-frame">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/faq" element={<FaqPage />} />
+            <Route path="/employee" element={<EmployeePage />} />
           </Routes>
         </BrowserRouter>
       </div>
