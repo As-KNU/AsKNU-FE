7e32473c6bec31df53ab34bb22c3cd8b4f1cec3b
Yea <dbs6463@gmail.com>
2025-11-30 21:55:06 +0900

    desktop version edit

--- asknu-fe/src/App.tsx
+++ asknu-fe/src/App.tsx
@@ -1,14 +1,3 @@
-// 1) 화면 높이 계산해서 CSS 변수에 넣기
-const setVh = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-};
-
-// 2) 최초 실행
-setVh();
-
-// 3) 화면 회전 / 주소창 변화 대응
-window.addEventListener("resize", setVh);
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import EmailAuthPage from "./pages/EmailAuthPage";
@@ -18,6 +7,7 @@ import "./index.css";
 
 export default function App() {
   return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="mobile-frame">
         <BrowserRouter>
           <Routes>
@@ -28,5 +18,6 @@ export default function App() {
           </Routes>
         </BrowserRouter>
       </div>
+    </div>
   );
 }
